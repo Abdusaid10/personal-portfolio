af7eea6d248dc5551b2688b6c645946d1db8d595
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2020-11-04 23:11:20 +0500

    add Navbar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
+import Navbar from '../components/Navbar';
 import '../assets/styles/App.css';
+import '../assets/styles/Navbar.css';
 
 const App = () => {
   return (
     <div className="App">
-     
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className="nav-container">
+      <div className="name">
+        A.A
+      </div>
+      <ul className="menu">
+        <li className="menu-item">Profile</li>
+        <li className="menu-item">About</li>
+        <li className="menu-item">Recent Work</li>
+        <li className="menu-item">Contact</li>
+      </ul>
+    </div>
+  )
+}
