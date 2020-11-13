fafc9d13659d9ab2fd41e80bc88fd7d2b82631d3
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2020-11-13 16:05:02 +0500

    style nav components

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
-import Navbar from '../components/Navbar';
-import '../assets/styles/App.css';
+import React from 'react';
+import Navbar from '../containers/Navbar';
+import '../assets/styles/reset.css';
+import '../assets/styles/index.css';
 import '../assets/styles/Navbar.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Navbar />
-    </div>
-  );
-}
+const App = () => (
+  <div className="app">
+    <Navbar />
+    <p>App</p>
+  </div>
+);
 
 export default App;
