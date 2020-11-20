066c85aebc3a75e21ae7cb383d7a12093579af0d
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2020-11-21 00:01:35 +0500

    add about component and style it

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Navbar from '../containers/Navbar';
+import About from './About';
 import '../assets/styles/reset.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/styles/index.css';
 import '../assets/styles/Navbar.css';
 
 const App = () => (
   <div className="app">
     <Navbar />
+    <About />
     <p>App</p>
   </div>
 );
