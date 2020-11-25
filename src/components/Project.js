fe0824be00fb0e364b3aa2610d5ea0c3747d299e
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2020-11-25 17:21:30 +0500

    fix layout of projects projects container, and style see project button, add images

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import '../assets/styles/Project.css';
 
 const Project = ({ project }) => (
   <div id="projectWrapper">
     <img className="project-img col-md-6" src={`${project.image}`} alt={`${project.name}`} />
-    <div className="col-md-6">
+    <div className="project-info col-md-6">
       <h2>{project.name}</h2>
       <p>{project.description}</p>
-      <div>
+      <div id="stackWrapper">
         {
           project.stack.map(s => (
             <span key={s}>{s}</span>
           ))
         }
       </div>
-      <button type="button">See project</button>
+      <button id="seeProject" type="button">
+        <spna className="p-1 m-1">See project</spna>
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M18.7423 10.6265L13.4834 5.36758L14.8976 3.95337L22.5707 11.6265L14.8976 19.2996L13.4834 17.8854L18.7422 12.6265H2L2 10.6265H18.7423Z" fill="white" />
+        </svg>
+      </button>
     </div>
   </div>
 );
--- src/containers/RecentWork.js
+++ src/containers/RecentWork.js
@@ -2,25 +2,45 @@ import React from 'react';
 import Project from '../components/Project';
 import weatherApp1 from '../assets/imgs/weatherApp1.png';
 import weatherApp2 from '../assets/imgs/weatherApp2.png';
+import devocean from '../assets/imgs/devocean_feed.png';
+import '../assets/styles/Project.css';
 
 const RecentWork = () => {
   const projects = [
     {
       name: 'Weather App',
-      description: 'lorem ipsum',
+      description: 'Weather app shows the weather information of the user\'s city when the page is loaded, the user can also search for and view the forecast in cities worldwide.',
       image: `${weatherApp1}`,
       stack: ['HTML', 'CSS', 'JavaScript'],
     },
     {
-      name: 'Weather1 App',
+      name: 'Devocean',
+      description: 'Social network for remote developers and learners',
+      image: `${devocean}`,
+      stack: ['Rails', 'JavaScript', 'HTML', 'CSS'],
+    },
+    {
+      name: 'Bookstore',
       description: 'lorem ipsum',
       image: `${weatherApp2}`,
-      stack: ['HTML', 'CSS', 'JavaScript'],
+      stack: ['Rails', 'React', 'Redux', 'JavaScript', 'HTML', 'CSS'],
+    },
+    {
+      name: 'Meal Recipes',
+      description: 'Get the recipe and instructions for the meal you want to cook',
+      image: `${weatherApp2}`,
+      stack: ['React', 'Redux', 'JavaScript', 'HTML', 'CSS'],
+    },
+    {
+      name: 'Car Booking API',
+      description: 'REST API with JWT authentication and two types of users. Admin users can perform CRUD operations on the following data:  car, category, manufacturer, while signed-up users can only book a car.',
+      image: `${weatherApp2}`,
+      stack: ['Rails', 'React', 'Redux', 'JavaScript', 'HTML', 'CSS'],
     },
   ];
 
   return (
-    <div id="projects-container">
+    <div id="projectsContainer">
       {
         projects.map(project => (
           <Project key={project.name} project={project} />
