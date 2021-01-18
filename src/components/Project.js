4cc9bd5a62264200437a42e72702cc972ca61ef9
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2021-01-18 08:47:15 +0500

    fix style of see project button

--- src/components/Project.js
+++ src/components/Project.js
@@ -17,7 +17,7 @@ const Project = ({ project }) => (
       <div className="links">
         <a href={project.srcLink} className="see-project" target="_blank" rel="noreferrer">
           <spna className="p-1 m-1">See source</spna>
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M18.7423 10.6265L13.4834 5.36758L14.8976 3.95337L22.5707 11.6265L14.8976 19.2996L13.4834 17.8854L18.7422 12.6265H2L2 10.6265H18.7423Z" fill="white" />
           </svg>
         </a>
